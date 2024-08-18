ad9c0081186c3073c5a32c1f14c12cec98578520
Artyom Korobkin <carlionminon@gmail.com>
2024-08-18 22:46:48 +0300

    update

--- events/models.py
+++ events/models.py
@@ -7,7 +7,7 @@ class Event(models.Model):
     short_title = models.CharField(max_length=settings.LIMITS("short_title"), unique=True)
     title = models.CharField(max_length=settings.LIMITS("title"))
     description = models.TextField(default = "Отсутствует")
-    dates = models.CharField(default = datetime.date.today)
+    dates = models.CharField(default = datetime.date.today, max_length=150)
     preparations = models.TextField(default = "Отсутствует")
     info = models.TextField(default = "Отсутствует")
     FAQ = models.TextField(default = "Отсутствует")
